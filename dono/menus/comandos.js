8acdb3c62e492850ebd37f2c411abb4bb0bf389a
Vemonbot2 <renanluizvargasbr2005@gmail.com>
2026-01-16 14:17:22 -0300

    Create comandos.js

---
+++ dono/menus/comandos.js
@@ -0,0 +1,16 @@
+module.exports = `
+╭━━━〔 🤖 VEMON BOT 〕━━━╮
+┃ 👑 Dono: Renan
+┃ ⚡ Prefixo: !
+┃
+┃ 📂 MENUS
+┃ • !menuadm
+┃ • !menudono
+┃ • !menuzoeira
+┃ • !menufig
+┃ • !menutinder
+┃ • !menunsfw
+┃ • !menulogos
+┃ • !menualuguel
+╰━━━━━━━━━━━━━━━━━━━━╯
+`
